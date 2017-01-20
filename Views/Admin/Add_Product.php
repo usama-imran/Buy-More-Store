f8d5f650f5c598959955e7c47109204859f86cf9
usama-imran <usama.imran@rolustech.com>
2017-01-20 17:53:31 +0500

    New file to add product created

---
+++ Views/Admin/Add_Product.php
@@ -0,0 +1,148 @@
+<?php 
+
+?>
+<html>
+	<head>
+		<title>Add Product</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="../Sources/JS/bootstrap-filestyle.min.js"> </script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- Script to enable and disable the associated product dropdown list -->
+		<link rel="stylesheet" href="../Sources/CSS/form_style.css" />  
+		<script type="text/javascript" src="../Sources/JS/pro_form_validation.js"></script>
+		<!-- Javascript to enable & disable associated products  -->
+		
+		<script type="text/javascript">
+		// Enable / Disable associated products
+		function disable_assoc_products()
+		{
+			var drop_down = document.getElementById("associated_product");
+	        drop_down.setAttribute("disabled", true);
+	        drop_down.style.visibility = 'hidden';
+		}
+		function enable_assoc_products() 
+		{
+			var check_box = document.getElementById("associated_product_enable");
+			var drop_down = document.getElementById("associated_product");
+			if(check_box.checked)
+			{
+				drop_down.removeAttribute("disabled");
+				drop_down.style.visibility = 'visible';	
+			}
+			else if(check_box.checked == false)
+			{
+				drop_down.setAttribute("disabled", true);
+				drop_down.style.visibility = 'hidden';	
+			}
+		}
+		</script>
+	</head>
+	<body onload="disable_assoc_products()">
+	<div class="container">
+	<div class="row">
+		<?php include 'views/templates/navbar.php';?>
+	</div>
+	<fieldset class="well">
+		<legend class="well" style="width: 160px">Add Product</legend>
+		<form action="add_product" method="post" id="pro_form" enctype="multipart/form-data"> 
+			<table>
+				<tr>
+					<td>Name:</td><td><input type="text" name="name" id="name" class="input_fields"></td>
+					<td><i><p id="name_detail" class="error_text"></p></i></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>Price:</td><td><input type="number" name="price" id="price" class="input_fields"></td>
+					<td><i><p id="price_detail" class="error_text"></p></i></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>Quantity:</td><td><input type="number" name="quantity" id="quantity" class="input_fields"></td>
+					<td><i><p id="quantity_detail" class="error_text"></p></i></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>Image:</td><td><input type="file" name="pro_image" id="pro_image" class="input_fields filestyle"  data-classButton="btn btn-primary" data-input="false" data-classIcon="icon-plus" data-buttonText="Select Image."></td>
+					<td><i><p id="image_detail" class="error_text"></p></i></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>Description:</td><td><textarea rows="4" cols="23" class="input_fields" name="description" style="resize:none" id="description"></textarea></td>
+					<td><i><p id="description_detail" class="error_text"></p></i></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>Category</td>
+					<td>
+						<select name="category" id="category" class="input_fields">
+						<option value="null" >--Select Category--</option>
+						<?php 
+						foreach ($categories as $value)
+						{ 
+						?>
+						<option value="<?php echo $value['cat_id'] ;?>">
+						<?php 
+							echo $value['cname'];
+							echo "</option>";
+						}
+						?>
+						</select>
+					</td>
+				<td><i><p id="category_detail" ></p></i></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>Associated Product</td>
+					<td>
+						<input type="hidden" name="associated_product_enable"  value="0">
+						<input type="checkbox"  name="associated_product_enable" id="associated_product_enable" value="1" onclick = "enable_assoc_products()">
+						<select name="associated_product" id="associated_product"  class="hidden_dropdowns" >
+						<option>--Select Product--</option>
+						<?php 
+							foreach ($products as $value)
+							{ 
+							?>
+							<option value="<?php echo $value['product_id'] ;?>">
+							<?php 
+								echo $value['name'];
+								echo "</option>";
+							}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>
+					Active:</td><td><input type="checkbox"  name="is_active" value="1">
+					</td><td><input type="hidden" name="is_active" value="0">
+					</td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td></td><td><input type="hidden" name="created_by" value="<?php echo htmlspecialchars($logged_in_userID); ?>"></td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td></td>
+					<td>
+						<button type="submit" class="btn btn-info" aria-label="Left Align">
+							<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>Add
+						</button>
+						<a href="Products" class="btn btn-warning">
+							<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Cancel
+						</a>		
+					</td>
+				</tr>
+			</table>
+		</form>
+	</fieldset>
+	
+	</div>
+	</body>
+
+</html>
