17789b229f8f3bb7bc6b16342a94fbfe434666fb
usama-imran <usama.imran@rolustech.com>
2017-01-30 12:12:32 +0500

    Modified

--- home.php
+++ home.php
@@ -5,28 +5,18 @@ define('BASE_URL','http://localhost:8080/BuyMore/');
 require_once 'database.php';
 function __autoload($class_name)
 {	
-	try 
+	
+	if(is_file("Libs/".$class_name.".php"))
 	{
-		if(is_file("Libs/".$class_name.".php"))
-		{
-			include_once "Libs/".$class_name.".php";
-		}
-		else if (is_file("Models/".$class_name.".php"))
-		{
-			include_once "Models/".$class_name.".php";
-		}
-		else if (is_file("Controllers/".$class_name.".php"))
-		{
-			include_once "Controllers/".$class_name.".php";
-		}
-		else 
-		{
-			throw new Exception("Error");
-		}
+		include_once "Libs/".$class_name.".php";
 	}
-	catch (Exception $e)
+	else if (is_file("Models/".$class_name.".php"))
 	{
-		echo $e->getMessage();
+		include_once "Models/".$class_name.".php";
+	}
+	else if (is_file("Controllers/".$class_name.".php"))
+	{
+		include_once "Controllers/".$class_name.".php";
 	}
 }
 new Bootstrap();
